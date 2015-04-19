b9f5570974de30a27b2c015044a42e4b1e9c95aa
verybadsoldier <vbs@springrts.de>
2015-04-19 21:59:15 +0200

    improved start script

--- fhem.sh
+++ fhem.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 HOST=localhost
 PORT=7072
-DEVICE=fl_airSensor
+DEVICE=sz_airSensor
 READING=airQuality
 USER=air
 
 COMMAND=./airsensor
-PARAMS_PRE="-d -i 120 -c "
+PARAMS_PRE="-e 5 -i 120 -c "
 USER_CMD="echo \"setreading $DEVICE $READING %d\" | nc -w10 $HOST $PORT"
 
-start-stop-daemon -b -c $USER -S -x $COMMAND -- $PARAMS_PRE "$USER_CMD"
+start-stop-daemon -m -p /var/run/airsensor.pid -b -c $USER -S -x $COMMAND -- $PARAMS_PRE "$USER_CMD"
