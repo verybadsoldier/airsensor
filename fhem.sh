604eef119fae6feb2c3cdb8142bd6e61780ace04
verybadsoldier <vbs@springrts.de>
2015-04-21 22:14:55 +0200

    airsensor: start script output to dev null

--- fhem.sh
+++ fhem.sh
@@ -9,4 +9,8 @@ COMMAND=./airsensor
 PARAMS_PRE="-e 5 -i 120 -c "
 USER_CMD="echo \"setreading $DEVICE $READING %d\" | nc -w10 $HOST $PORT"
 
-start-stop-daemon -m -p /var/run/airsensor.pid -b -c $USER -S -x $COMMAND -- $PARAMS_PRE "$USER_CMD"
+# either use this to start airsensor in shell-observered respawn loop
+#(while true; do sudo -u $USER $COMMAND $PARAMS_PRE "$USER_CMD" > /dev/null 2>&1;sleep 1;done;)&
+
+# or us start stop
+#start-stop-daemon -m -p /var/run/airsensor.pid -b -c $USER -S -x $COMMAND -- $PARAMS_PRE "$USER_CMD"
