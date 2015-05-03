9d35abc9123e399cc9b6017d987f5de031547047
verybadsoldier <vbs@springrts.de>
2015-05-03 13:53:47 +0200

    improved start scripts

--- fhem.sh
+++ fhem.sh
@@ -1,16 +1,18 @@
 #!/bin/sh
-HOST=localhost
+HOST=fhem
 PORT=7072
-DEVICE=sz_airSensor
+DEVICE=$1
 READING=airQuality
-USER=air
+USER=tc
+PID=/var/run/airsensor.pid
 
-COMMAND=./airsensor
-PARAMS_PRE="-e 5 -i 120 -c "
+COMMAND=/mnt/MASS2/fhem-tc/airsensor/airsensor
+COMMAND_ARM=/mnt/MASS2/fhem-tc/airsensor/airsensor_arm
+PARAMS_PRE="-l 15000 -e 5 -i 120 -c "
 USER_CMD="echo \"setreading $DEVICE $READING %d\" | nc -w10 $HOST $PORT"
 
-# either use this to start airsensor in shell-observered respawn loop
-#(while true; do sudo -u $USER $COMMAND $PARAMS_PRE "$USER_CMD" > /dev/null 2>&1;sleep 1;done;)&
-
-# or us start stop
-#start-stop-daemon -m -p /var/run/airsensor.pid -b -c $USER -S -x $COMMAND -- $PARAMS_PRE "$USER_CMD"
+if [ -z "$2" ]; then 
+  start-stop-daemon -m -p $PID -b -c $USER -S -x $COMMAND_ARM -- $PARAMS_PRE "$USER_CMD"
+else 
+  (while true; do sudo -u $USER $COMMAND $PARAMS_PRE "$USER_CMD" > /dev/null 2>&1;sleep 1;done;)&
+fi
